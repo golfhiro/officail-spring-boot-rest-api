97a534a5c2fb7de6ff5e0c178c7c48f9203a8abb
golfhiro <tamaruhiroki@team-lab.com>
2024-05-26 20:53:32 +0900

    feat: 起動時のバナーを非表示

--- src/main/kotlin/org/example/blog/OfficialKotlinSpringWebApplication.kt
+++ src/main/kotlin/org/example/blog/OfficialKotlinSpringWebApplication.kt
@@ -1,5 +1,6 @@
 package org.example.blog
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class OfficialKotlinSpringWebApplication
 
 fun main(args: Array<String>) {
-    runApplication<OfficialKotlinSpringWebApplication>(*args)
+    runApplication<OfficialKotlinSpringWebApplication>(*args) {
+        setBannerMode(Banner.Mode.OFF)
+    }
 }
